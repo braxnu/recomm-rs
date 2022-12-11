58691c93630cabc87a6ee6ab958e5b945a08e16d
Karol Baczewski <braxnu@gmail.com>
2022-12-11 11:50:24 +0000

    integration tests

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,19 @@ use serde_json::json;
 
 type ProductId = String;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 struct Product {
     id: ProductId,
     name: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 struct OrderItem {
     product: Product,
     quantity: u16,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 struct Order {
     id: String,
     items: Vec<OrderItem>,
@@ -83,7 +83,10 @@ async fn delete_orders(state: web::Data<Mutex<AppState>>) -> impl Responder {
 }
 
 #[get("/products/{product_id}/bought_together")]
-async fn get_bought_together(state: web::Data<Mutex<AppState>>, path: web::Path<ProductId>) -> impl Responder {
+async fn get_bought_together(
+    state: web::Data<Mutex<AppState>>,
+    path: web::Path<ProductId>
+) -> impl Responder {
     let product_id = path.into_inner();
     let mut product_count_map: HashMap<ProductId, u32> = HashMap::new();
     let orders = & state.lock().unwrap().orders;
@@ -106,8 +109,6 @@ async fn get_bought_together(state: web::Data<Mutex<AppState>>, path: web::Path<
 
     let serached_product_index = entries.iter()
         .position(|p| {
-            println!("{}", json!({"p": p}));
-
             p.0 == product_id
         })
         .unwrap();
@@ -149,4 +150,327 @@ async fn main() -> std::io::Result<()> {
         .bind(("127.0.0.1", 4600))?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Mutex;
+
+    use actix_web::{
+        http::header::ContentType,
+        test::{self, read_body_json},
+        web::{self, Bytes},
+        App,
+        body::{MessageBody, BoxBody}
+    };
+    use serde_json::json;
+    use crate::{
+        AppState,
+        Order,
+        OrderItem,
+        Product,
+        delete_orders,
+        post_order, get_bought_together,
+    };
+
+    use super::{get_orders};
+
+    #[actix_web::test]
+    async fn test_get_orders() {
+        let orders: Vec<Order> = vec![
+            Order {
+                id: "aaa".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "ccc".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    }
+                ],
+            }
+        ];
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(
+                    Mutex::new(
+                        AppState {
+                            orders: orders.clone(),
+                        }
+                    )
+                ))
+                .service(get_orders)
+        ).await;
+
+        let req = test::TestRequest::default()
+            .uri("/orders")
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let body: Vec<Order> = read_body_json(resp).await;
+
+        assert!(body[0] == orders[0]);
+    }
+
+    #[actix_web::test]
+    async fn test_delete_orders() {
+        let orders: Vec<Order> = vec![
+            Order {
+                id: "aaa".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "ccc".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    }
+                ],
+            },
+        ];
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(
+                    Mutex::new(
+                        AppState {
+                            orders: orders.clone(),
+                        }
+                    )
+                ))
+                .service(get_orders)
+                .service(delete_orders)
+        ).await;
+
+        let req = test::TestRequest::delete()
+            .uri("/orders")
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let req = test::TestRequest::get()
+            .uri("/orders")
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let body: Vec<Order> = read_body_json(resp).await;
+
+        assert!(body == vec![]);
+    }
+
+    #[actix_web::test]
+    async fn test_post_order() {
+        let orders: Vec<Order> = vec![];
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(
+                    Mutex::new(
+                        AppState {
+                            orders: orders.clone(),
+                        }
+                    )
+                ))
+                .service(get_orders)
+                .service(post_order)
+        ).await;
+
+        let req = test::TestRequest::post()
+            .uri("/orders")
+            .insert_header(ContentType::json())
+            .set_payload(
+                json!({
+                    "id": "aaa",
+                    "items": [
+                        {
+                            "product": {
+                                "id": "ccc",
+                                "name": "box",
+                            },
+                            "quantity": 3,
+                        }
+                    ],
+                }).to_string()
+            )
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let req = test::TestRequest::get()
+            .uri("/orders")
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let body: Vec<Order> = read_body_json(resp).await;
+
+        assert!(body == vec![
+            Order {
+                id: "aaa".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "ccc".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    }
+                ],
+            },
+        ]);
+    }
+
+    #[actix_web::test]
+    async fn test_bought_together() {
+        let orders: Vec<Order> = vec![
+            Order {
+                id: "o-1".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "sample".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                    OrderItem {
+                        product: Product {
+                            id: "aaa".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                    OrderItem {
+                        product: Product {
+                            id: "bbb".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                    OrderItem {
+                        product: Product {
+                            id: "ccc".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                ],
+            },
+            Order {
+                id: "o-2".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "sample".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                    OrderItem {
+                        product: Product {
+                            id: "aaa".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                    OrderItem {
+                        product: Product {
+                            id: "ccc".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                ],
+            },
+            Order {
+                id: "o-3".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "sample".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                    OrderItem {
+                        product: Product {
+                            id: "ccc".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                ],
+            },
+            Order {
+                id: "o-4".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "ddd".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                ],
+            },
+            Order {
+                id: "o-5".to_string(),
+                items: vec![
+                    OrderItem {
+                        product: Product {
+                            id: "ddd".to_string(),
+                            name: "box".to_string(),
+                        },
+                        quantity: 3,
+                    },
+                ],
+            },
+        ];
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(
+                    Mutex::new(
+                        AppState {
+                            orders: orders.clone(),
+                        }
+                    )
+                ))
+                .service(get_bought_together)
+        ).await;
+
+        let uri = format!("/products/{}/bought_together", "sample");
+
+        let req = test::TestRequest::get()
+            .uri(uri.as_str())
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let body: Vec<String> = read_body_json(resp).await;
+
+        assert_eq!(body, vec![
+            "ccc".to_string(),
+            "aaa".to_string(),
+            "bbb".to_string(),
+        ]);
+
+        assert!(!body.contains(&"ddd".to_string()));
+    }
 }
